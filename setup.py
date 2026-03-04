5fb395c88d60079b1744e0079861745be6b8ed64
Omer <96557499+OmericoN@users.noreply.github.com>
2026-03-04 13:51:21 +0100

    setup fixed

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = [
+        line.strip() for line in f
+        if line.strip() and not line.startswith("#")
+    ]
 setup(
     name="idrd_pipeline",
     version="0.1.0",
     packages=find_packages(where="src"),
+    install_requires=requirements,
     package_dir={"": "src"},
 )
